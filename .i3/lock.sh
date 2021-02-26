654b3cb2c164e4ce82bf60b40b199f453af00f58
stan-guer <canosonic@gmail.com>
2021-02-26 14:09:36 +0100

    polybar: use xscreenaver
    have script use xscreenaver

--- .i3/lock.sh
+++ .i3/lock.sh
@@ -1,6 +1,8 @@
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center  -composite -matte /tmp/screen.png
+mkdir /tmp/lock
+scrot /tmp/lock/screen.png
+convert /tmp/lock/screen.png -scale 10% -scale 1000% /tmp/lock/screen.png
+[[ -f $1 ]] && convert /tmp/lock/screen.png $1 -gravity center  -composite -matte /tmp/lock/screen.png
 #dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
-i3lock -i /tmp/screen.png
-rm /tmp/screen.png
+xscreensaver-command -lock
+#i3lock -i /tmp/screen.png
+#rm /tmp/screen.png
