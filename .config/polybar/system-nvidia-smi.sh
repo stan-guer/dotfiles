1c5bd29b2d6722330f7d6d6645bcaea988433749
Stan Guerassimov <9304923+stan-guer@users.noreply.github.com>
2026-02-10 12:23:18 +0100

    update_polybar_config

--- .config/polybar/system-nvidia-smi.sh
+++ .config/polybar/system-nvidia-smi.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-echo "GPU $(nvidia-smi --query-gpu=utilization.gpu,memory.used --format=csv,noheader)"
+GPU_INFO=$(nvidia-smi --query-gpu=utilization.gpu,memory.used --format=csv,noheader 2>/dev/null)
+
+if [ $? -eq 0 ] && [ -n "$GPU_INFO" ]; then
+    echo "GPU $GPU_INFO"
+else
+    echo "GPU ERR"
+fi
