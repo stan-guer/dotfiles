a9ac537a41505c949b10e87903fb1a6d4aaaa2b9
Stan Guerassimov <9304923+stan-guer@users.noreply.github.com>
2026-05-05 15:22:19 +0200

    fix_polybar_multiscreen

--- .config/polybar/launch_polybar_multi.sh
+++ .config/polybar/launch_polybar_multi.sh
@@ -1,13 +1,38 @@
-# Terminate already running bar instances
-killall -q polybar
+#!/bin/sh
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+CONFIG="$HOME/.config/polybar/config"
+BAR_NAME="${1:-fullbar}"
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload -c ~/.config/polybar/config fullbar  &
+killall -q polybar 2>/dev/null || true
+
+while pgrep -u "$(id -u)" -x polybar >/dev/null 2>&1; do
+  sleep 0.2
+done
+
+monitors=""
+if command -v polybar >/dev/null 2>&1; then
+  monitors="$(polybar --list-monitors 2>/dev/null | cut -d: -f1)"
+fi
+
+if [ -z "$monitors" ] && command -v xrandr >/dev/null 2>&1; then
+  monitors="$(xrandr --query 2>/dev/null | awk '
+    $2 == "connected" {
+      for (i = 3; i <= NF; i++) {
+        if ($i ~ /^[0-9]+x[0-9]+[+-]/) {
+          print $1
+          break
+        }
+      }
+    }')"
+fi
+
+if [ -n "$monitors" ]; then
+  tray_position=right
+  for monitor in $monitors; do
+    MONITOR="$monitor" TRAY_POSITION="$tray_position" polybar -c "$CONFIG" "$BAR_NAME" &
+    tray_position=none
+    sleep 1
   done
 else
-  polybar --reload -c  ~/.config/polybar/config fullbar &
+  TRAY_POSITION=right polybar -c "$CONFIG" "$BAR_NAME" &
 fi
